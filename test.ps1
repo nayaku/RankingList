8abfa2c4d3f48fa7ed75674f00c0ff7bf68b4746
nayaku <736248591@qq.com>
2026-03-23 11:14:47 +0800

    更新批测试脚本

--- test.ps1
+++ test.ps1
@@ -1,11 +1,13 @@
 # cd "RankingListTestNew\bin\Release\net10.0"
 $program = ".\RankingListTestNew.exe"
 $jsonDir = ".\Test\"
+$rankingName = "BucketLinkedListRankingList"
 
+Write-Host $rankingName
 $jsonFiles = Get-ChildItem -Path $jsonDir -Filter "*.json" -File
 
 foreach ($file in $jsonFiles) {
     $baseName = $file.BaseName
     Write-Host "Testing: $baseName"
-    & $program test BucketBRTreeRankingList -t $baseName
+    & $program test $rankingName --base BucketBRTreeRankingList -t $baseName
 }
